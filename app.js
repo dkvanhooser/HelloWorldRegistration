1ef4d4bbe4d2ab9ee66cd3e296e87b4c0ce64040
dkvanhooser <dkvanhooser@gmail.com>
2018-08-20 18:56:37 -0400

    all working except vieiwng al registered users

--- app.js
+++ app.js
@@ -3,11 +3,36 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const Joi = require('joi');
+ 
+var userSchema = Joi.object().keys({
+    firstName: Joi.string().alphanum().min(2).max(30).required(),
+    lastName: Joi.string().alphanum().min(2).max(30).required(),
+    address1: Joi.string().alphanum().min(3).max(50).required(),
+    address2: Joi.string().alphanum().min(3).max(50).required(),
+    city: Joi.string().alphanum().min(2).max(30).required(),
+    state: Joi.string().alphanum().min(2).max(2).required(),
+    zipCode: Joi.string().regex(/^\d{5}(?:[-\s]\d{4})?$/).alphanum().min(5).max(10).required(),
+});
 
 var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+// in latest body-parser use like below.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//db connection
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'Das boot123',
+  database : 'registration'
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +44,42 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//routing
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.post('/register', function(req, res, next) {
+	const result = Joi.validate(req.body, userSchema);
+
+	if(result.error){
+		next(result.error);
+	} else {
+		var insertUserStatement = "insert into users values('" + req.body.firstName + "', '" + req.body.lastName + "' , '" + req.body.address1 + "', '" + req.body.address2 + "' , '" + req.body.city +"', '" + req.body.state + "', '" + req.body.zipCode +"', 'US' , NOW());";
+
+		connection.query(insertUserStatement, function (err, rows, fields) {
+  			if (err) {
+  				throw err
+  			}else{
+  				res.render('confirmation');
+  			}
+		});
+	}
+});
+
+app.get('/users', function(req, res, next) {
+	var selectUserStatement = 'Select * from users;';
+	var users = ["WOOF"];
+	connection.query(selectUserStatement, function (err, rows, fields) {
+		users = rows;
+	    console.log("users:")
+		console.log(users);
+  		if (err) throw errs;
+		res.render('registrations', {users});	
+		});
+});
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
